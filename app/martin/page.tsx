707da720a9ce822c81cd57a6d8ad246364b4dad8
Martin Crochelet <martin_crochelet@mckinsey.com>
2023-08-25 20:07:38 +0100

    feat: home page with support for light and dark mode

---
+++ app/martin/page.tsx
@@ -0,0 +1,9 @@
+export default function Martin() {
+
+  return (
+    <main className="flex flex-wrap place-content-start place-items-start justify-center">
+      <h1 className="text-4xl text-bold m-8">Martin</h1>
+      <h2 className="text-2xl m-8">Hello there, this is under construction</h2>
+    </main>
+  )
+}
